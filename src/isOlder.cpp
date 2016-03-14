39f9b7d492ce93f885de2b741434340ededb0a66
anushaavuthu <anushaavuthu@missionrnd.com>
2016-03-14 19:18:36 +0530

    Submit Commit

--- src/isOlder.cpp
+++ src/isOlder.cpp
@@ -13,7 +13,123 @@ ERROR CASES: Return -1 if any DOB is invalid.
 NOTES: 		Don't use any built-in C functions for comparisions. You are free to write any helper functions.
 */
 
-int isOlder(char *dob1, char *dob2) {
+int length(char* str){
+	int i;
+	for (i = 0; str[i] != '\0'; i++);
+	return i;
+}
 
+int isOlder(char *dob1, char *dob2) {
+	int i,j, date1 = 0, date2 = 0, month1 = 0, month2 = 0, year1 = 0, year2 = 0;
+	if (length(dob1) == 10 && length(dob2) == 10){
+		for (i = 0; i < 10; i++){
+			if (i != 2 && i != 5){
+				if ((dob1[i] >= '0' && dob1[i] <= '9') && (dob2[i] >= '0' && dob2[i] <= '9')){
+					
+					//conversion
+					for (j = 0; j < 2; j++){
+						date1 = (date1 * 10) + (dob1[j] - '0');
+						date2 = (date2 * 10) + (dob2[j] - '0');
+					}
+					j++;
+					while (j < 5){
+						month1 = (month1 * 10) + (dob1[j] - '0');
+						month2 = (month2 * 10) + (dob2[j] - '0');
+						j++;
+					}
+					j++;
+					while (j < 10){
+						year1 = (year1 * 10) + (dob1[j] - '0');
+						year2 = (year2 * 10) + (dob2[j] - '0');
+						j++;
+					}
+
+					//validation
+					if (month1 < 13 && month2 < 13){
+						if (month1 == 1 || month1 == 3 || month1 == 5 ||month1 == 7 || month1 == 8 || month1 == 10 || month1 == 12){
+							if (date1 >= 32){
+								return -1;
+							}
+						}
+						else if (month1 == 4 || month1 == 6 || month1 == 9 || month1 == 11){
+							if (date1 >= 31){
+								return -1;
+							}
+						}
+						else{
+							if (year1 % 4 == 0){
+								if (date1 >= 30){
+									return -1;
+								}
+							}
+							else{
+								if (date1 >= 29){
+									return -1;
+								}
+							}
+						}
+						if (month2 == 1 || month2 == 3 || month2 == 5 || month2 == 7 || month2 == 8 || month2 == 10 || month2 == 12){
+							if (date2 >= 32){
+								return -1;
+							}
+						}
+						else if (month2 == 4 || month2 == 6 || month2 == 9 || month2 == 11){
+							if (date2 >= 31){
+								return -1;
+							}
+						}
+						else{
+							if (year2 % 4 == 0){
+								if (date2 >= 30){
+									return -1;
+								}
+							}
+							else{
+								if (date2 >= 29){
+									return -1;
+								}
+							}
+						}
+					}
+					else{
+						return -1;
+					}
+
+					//comparision
+					if (year1 < year2){
+						return 1;
+					}
+					else if (year2 < year1){
+						return 2;
+					}
+					else{
+						if (month1 < month2){
+							return 1;
+						}
+						else if(month2 < month1){
+							return 2;
+						}
+						else{
+							if (date1 < date2){
+								return 1;
+							}
+							else if (date2 < date1){
+								return 2;
+							}
+							else{
+								return 0;
+							}
+						}
+					}
+				}
+				else{
+					return -1;
+				}
+			}
+		}
+	}
+	else{
+		return -1;
+	}
 	return 0;
 }
